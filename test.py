30f304bf63be68a8572ddb060916d0e71d7f1de0
Juanigomez <88172215+Juanigomez@users.noreply.github.com>
2022-12-01 20:16:56 -0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+
+header = st.container()
+inputs = st.container()
+dataset = st.container()
+
+def get_Data(filename):
+    data = pd.read_csv(filename)
+    return data
+
+with header:
+    st.title("Welcome to my test app")
+    st.text("In this project I am going to sketch the v2.2 of nmy business system")
+
+with inputs:
+    st.header("Data input")
+
+    Name = st.text_input("Enter customer name: ")
+    Address = st.text_input("Enter customer address: ")
+    Phone_Number = st.text_input("Enter customer phone number: ")
+
+    new_data = [[Name, Address, Phone_Number]]
+
+    df = pd.DataFrame(new_data)
+    if(st.button("Submit")):
+        df.to_csv('data/dataset.csv', mode='a', index=False, header=False)
+
+with dataset:
+    st.header("My datset")
+    st.text("On this dataset I am going to show customer, products and purchase information")
+
+    customer_data = get_Data('data/dataset.csv')
+    st.write(customer_data)
