b65824fc69f80766ef6b8084e547bba93ef46a65
Juanigomez <88172215+Juanigomez@users.noreply.github.com>
2022-12-02 14:41:24 -0300

    Update test.py

--- test.py
+++ test.py
@@ -34,7 +34,7 @@ def Customer():
 
         with col2:
 
-            st.subheader("My dataset")
+            st.subheader("Customer dataset")
             st.text("Table containing customer information: ")
 
             customer_data = get_Data('customers.csv')
@@ -43,7 +43,42 @@ def Customer():
 
 def Product():
 
-    st.warning("Page in progress...")
+    header = st.container()
+    inputs = st.container()
+
+    def get_Data(filename):
+        data = pd.read_csv(filename)
+        return data
+
+    with header:
+        st.title("Product Information")
+        st.text("In this page you are able to input and access product information.")
+
+    with inputs:
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+
+            st.subheader("Data input")
+
+            Name = st.text_input("Enter product name: ")
+            Stock = st.select_slider("Enter product stock: ")
+            Price = st.number_input("Enter product price: ")
+
+            new_data = [[Name, Stock, Price]]
+
+            df = pd.DataFrame(new_data)
+            if(st.button("Submit")):
+                df.to_csv('products.csv', mode='a', index=False, header=False)
+
+        with col2:
+
+            st.subheader("Product dataset")
+            st.text("Table containing product information: ")
+
+            product_data = get_Data('products.csv')
+            st.write(product_data)
 
 all_Pages = {
     "Customer": Customer,
