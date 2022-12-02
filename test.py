d5c14c1123be004a568dc2e73d9ab0f53bd0af0d
Juanigomez <88172215+Juanigomez@users.noreply.github.com>
2022-12-02 14:17:03 -0300

    Update test.py

--- test.py
+++ test.py
@@ -3,32 +3,37 @@ import pandas as pd
 
 header = st.container()
 inputs = st.container()
-dataset = st.container()
 
 def get_Data(filename):
     data = pd.read_csv(filename)
     return data
 
 with header:
-    st.title("Welcome to my test app")
-    st.text("In this project I am going to sketch the v2.2 of nmy business system")
+    st.title("Customer Information")
+    st.text("In this page you are able to input and access customer information.")
 
 with inputs:
-    st.header("Data input")
 
-    Name = st.text_input("Enter customer name: ")
-    Address = st.text_input("Enter customer address: ")
-    Phone_Number = st.text_input("Enter customer phone number: ")
+    col1, col2 = st.columns(2)
 
-    new_data = [[Name, Address, Phone_Number]]
+    with col1:
 
-    df = pd.DataFrame(new_data)
-    if(st.button("Submit")):
-        df.to_csv('dataset.csv', mode='a', index=False, header=False)
+        st.subheader("Data input")
 
-with dataset:
-    st.header("My dataset")
-    st.text("On this dataset I am going to show customer, products and purchase information")
+        Name = st.text_input("Enter customer name: ")
+        Address = st.text_input("Enter customer address: ")
+        Phone_Number = st.text_input("Enter customer phone number: ")
 
-    customer_data = get_Data('dataset.csv')
-    st.write(customer_data)
+        new_data = [[Name, Address, Phone_Number]]
+
+        df = pd.DataFrame(new_data)
+        if(st.button("Submit")):
+            df.to_csv('dataset.csv', mode='a', index=False, header=False)
+
+    with col2:
+
+        st.subheader("My dataset")
+        st.text("On this dataset I am going to show customer, products and purchase information")
+
+        customer_data = get_Data('dataset.csv')
+        st.write(customer_data)
