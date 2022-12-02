e1040f3743aa1b96dccd9cc869e1a04ff899e6ef
Juanigomez <88172215+Juanigomez@users.noreply.github.com>
2022-12-02 14:24:57 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,39 +1,56 @@
 import streamlit as st
 import pandas as pd
 
-header = st.container()
-inputs = st.container()
+def Customer():
 
-def get_Data(filename):
-    data = pd.read_csv(filename)
-    return data
+    header = st.container()
+    inputs = st.container()
 
-with header:
-    st.title("Customer Information")
-    st.text("In this page you are able to input and access customer information.")
+    def get_Data(filename):
+        data = pd.read_csv(filename)
+        return data
 
-with inputs:
+    with header:
+        st.title("Customer Information")
+        st.text("In this page you are able to input and access customer information.")
 
-    col1, col2 = st.columns(2)
+    with inputs:
 
-    with col1:
+        col1, col2 = st.columns(2)
 
-        st.subheader("Data input")
+        with col1:
 
-        Name = st.text_input("Enter customer name: ")
-        Address = st.text_input("Enter customer address: ")
-        Phone_Number = st.text_input("Enter customer phone number: ")
+            st.subheader("Data input")
 
-        new_data = [[Name, Address, Phone_Number]]
+            Name = st.text_input("Enter customer name: ")
+            Address = st.text_input("Enter customer address: ")
+            Phone_Number = st.text_input("Enter customer phone number: ")
 
-        df = pd.DataFrame(new_data)
-        if(st.button("Submit")):
-            df.to_csv('dataset.csv', mode='a', index=False, header=False)
+            new_data = [[Name, Address, Phone_Number]]
 
-    with col2:
+            df = pd.DataFrame(new_data)
+            if(st.button("Submit")):
+                df.to_csv('dataset.csv', mode='a', index=False, header=False)
 
-        st.subheader("My dataset")
-        st.text("On this dataset I am going to show customer, products and purchase information")
+        with col2:
 
-        customer_data = get_Data('dataset.csv')
-        st.write(customer_data)
+            st.subheader("My dataset")
+            st.text("Table containing customer information: ")
+
+            customer_data = get_Data('dataset.csv')
+            st.write(customer_data)
+
+
+def Product():
+
+    st.warning("Page in progress...")
+
+all_Pages = {
+    "Customer": Customer,
+    "Product": Product,
+}
+
+st.sidebar.title("Python Website")
+st.sidebar.header("Databse structure")
+selected_page = st.sidebar.selectbox("Select a page", all_Pages.keys())
+all_Pages[selected_page]()
